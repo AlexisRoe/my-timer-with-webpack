2be121133dad6a50b3984ece74396ddf7fe74ef3
AlexisRoe <A.Roehrling@gmx.de>
2020-10-27 20:58:09 +0100

    Initiale commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import "./app.css";
-import Button from "./components/Button";
 import Header from "./components/Header";
 import { createElement, styled } from "./utils/elements";
 
-const PrimaryButton = styled(Button, "bg-primary");
+// const PrimaryButton = styled(Button, "bg-primary");
 
 function App() {
-  const header = Header();
-
-  const main = createElement("main", {
-    innerText: "👋",
-  });
-
   const container = createElement("div", {
-    children: [
-      header,
-      main,
-      Button({ innerText: "Hello" }),
-      PrimaryButton({ innerText: "World" }),
-    ],
+    className: "container",
+    children: [],
   });
   return container;
 }
